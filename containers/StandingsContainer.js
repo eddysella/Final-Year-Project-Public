3a52e07d9bead4ad51398305e32f45936c845654
eddy <es463@sussex.ac.uk>
2019-11-20 16:16:36 +0000

    Standings Container + Component(Screen)

---
+++ containers/StandingsContainer.js
@@ -0,0 +1,72 @@
+import React, { PureComponent } from 'react';
+import { Button, BackHandler, AsyncStorage, ScrollView, View, ActivityIndicator, Text, Dimensions } from 'react-native';
+import SquareGrid from "react-native-square-grid";
+import { Avatar } from 'react-native-elements';
+import { getLeagueByID } from '../fetch/League';
+import { StandingsScreen } from '../screens/Standings'
+
+export default class StandingsContainer extends PureComponent {
+    constructor(props){
+        super(props);
+    }
+
+    state = {
+        loading: true,
+        leaguesList: [],
+    }
+
+    static navigationOptions = {
+        headerTitle: 'Standings',
+        headerRight:
+          <Button
+            onPress={() => alert('This is a button!')}
+            title="Edit"
+            color="black"
+          />
+        ,
+      };
+
+    async componentDidMount() {
+        try {
+            const arr = await AsyncStorage.getItem( 'PREFSTANDINGS' );
+            console.log( "Im getting called" );
+            if ( value !== null ) {
+                console.log( "Found saved standings" );
+                let collect = [];
+                for ( value in JSON.parse( arr ) ) {
+                    getLeagueByID( value ).then( data => collect.push(data));
+                }
+                this.setState({
+                    leaguesList: collect,
+                    loading: false,
+                });
+            }
+        } catch ( e ) {
+            try {
+                console.log( "Init default standings" );
+                await AsyncStorage.setItem( 'PREFSTANDINGS', JSON.stringify( [ 1 ] ) );
+                console.log( 'retrievingData' );
+                getLeagueByID( '1' ).then( data =>
+                    this.setState( {
+                        leaguesList: data,
+                        loading: false,
+                    } )
+                );
+            } catch ( error ) {
+                // Error saving data
+            }
+        }
+    }
+
+    render(){
+        if (this.state.isLoading) {
+            return (
+                <View style={{ flex: 1}}>
+                    <ActivityIndicator />
+                </View>
+            );
+        }else{
+            return <StandingsScreen leaguesList={this.state.leaguesList} navigation={this.props.navigation} />;
+        }
+    }
+}
---
+++ screens/Standings.js
@@ -0,0 +1,40 @@
+import React, { PureComponent } from 'react';
+import { Button, View, Text, ActivityIndicator} from 'react-native';
+import SquareGrid from "react-native-square-grid";
+import { Avatar } from 'react-native-elements';
+
+export const StandingsScreen = props => {
+
+    function renderItem(item, index) {
+        return (
+            <View style={{borderWidth: 3, margin: 5, padding: 10, alignItems: 'center',  alignSelf: 'stretch'}}>
+                <Avatar
+                    size = 'large'
+                    source={{ uri: `${item.logo}`}}
+                    rounded
+                />
+                <Text h3>
+                    {item.name}
+                </Text>
+            </View>
+        );
+    }
+
+    return (
+        <View style={{flex:1}}>
+            <SquareGrid
+                rows={0}
+                columns={3}
+                items={props.leaguesList}
+                renderItem={renderItem}
+            />
+            <Button
+                title="Go to Details"
+                onPress={() => props.navigation.navigate('Inner')}
+            />
+        </View>
+    );
+}
+
+
+//get seasons available from leagueID then get newest one
