a0c64636f2c63391ab039b638be782c44c6a56da
eddy <es463@sussex.ac.uk>
2019-11-20 23:24:15 +0000

    Changed flex proportions to make item size of scrolling menu bar fit

--- screens/Fixtures.js
+++ screens/Fixtures.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import { ExpoConfigView } from '@expo/samples';
+import { Button, BackHandler, AsyncStorage, ScrollView, View, ActivityIndicator, Text, Dimensions } from 'react-native';
 
 export const FixturesScreen = props => {
-          return <ExpoConfigView />;
+          return (<View style={{flex:8}}>
+          </View>);
 
 }
