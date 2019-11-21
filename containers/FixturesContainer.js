9a0c0014938538dfdd26b2ff6bd6a80d132ed468
eddy <es463@sussex.ac.uk>
2019-11-21 17:51:55 +0000

    Fixtures Logic working, fetch works and passes to screen.

--- containers/FixturesContainer.js
+++ containers/FixturesContainer.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react';
 import { Button, BackHandler, AsyncStorage, ScrollView, View, ActivityIndicator, Text, Dimensions } from 'react-native';
 import SquareGrid from "react-native-square-grid";
 import { Avatar } from 'react-native-elements';
-import { getLeagueByID } from '../fetch/League';
+import { getAllFixturesByDate } from '../fetch/Fixtures';
 import { FixturesScreen } from '../screens/Fixtures';
 import FixturesTopBarContainer from '../containers/FixturesTopBarContainer';
 
@@ -13,11 +13,51 @@ export default class FixturesContainer extends PureComponent {
 
     state = {
         loading: true,
+        leaguesDict: undefined,
     }
 
     static navigationOptions = {
         title: 'Fixtures',
     };
+
+    async componentDidMount() {
+        const date = new Date();
+        date.setDate(date.getDate());
+        var dd = String(date.getDate()).padStart(2, '0');
+        var mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var year = String(date.getFullYear());
+        dateString = year + '-' + mm + '-' + dd;
+        console.log(dateString);
+
+        collect={};
+
+        getAllFixturesByDate( dateString ).then( data => {
+            data = data.api;
+            fixtures = data.fixtures;
+            fixtures.forEach( fixture => {
+                league = fixture.league;
+                leagueName = league.name;
+                if (!(leagueName in collect)) {
+                    collect[leagueName] = [];
+                }
+                collect[leagueName].push({
+                    flag:league.logo,
+                    id:fixture.fixture_id,
+                    status:fixture.statusShort,
+                    elapsed:fixture.elapsed,
+                    homeTeam:fixture.homeTeam,
+                    awayTeam:fixture.awayTeam,
+                    goalsHome:fixture.goalsHomeTeam,
+                    goalsAway:fixture.goalsAwayTeam,
+                });
+            });
+
+            this.setState({
+                leaguesDict: collect,
+                loading: false,
+            });
+        });
+    }
 
     render(){
         if (this.state.isLoading) {
@@ -30,7 +70,7 @@ export default class FixturesContainer extends PureComponent {
             return (
                 <View style={{ flex: 1}}>
                     <FixturesTopBarContainer TopBarFlex={1}/>
-                    <FixturesScreen ScreenFlex={8}/>
+                    <FixturesScreen ScreenFlex={8} leaguesDict={this.state.leaguesDict}/>
                 </View>
             );
         }
