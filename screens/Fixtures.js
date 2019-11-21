8982241f8ac4c89e9f91cd06e79613c485813d83
eddy <es463@sussex.ac.uk>
2019-11-21 22:12:15 +0000

    Fixed display issue on fixtures.

--- screens/Fixtures.js
+++ screens/Fixtures.js
@@ -2,15 +2,27 @@ import React, {Component} from 'react';
 import { ExpoConfigView } from '@expo/samples';
 import {Button, BackHandler, AsyncStorage, FlatList, View, ActivityIndicator, Text, Dimensions } from 'react-native';
 import {Card} from 'react-native-elements';
+import { scale, verticalScale, moderateScale } from 'react-native-size-matters';
+
+const screenWidth = Math.round(Dimensions.get('window').width);
+const screenHeight = Math.round(Dimensions.get('window').height);
 
 export const FixturesScreen = props => {
 
+    let itemWidth = screenWidth - scale(screenWidth/5);
+    let itemHorizontalPadding = (itemWidth/5);
+    let itemVerticalPadding = scale(15);
+
     function renderItem(fixture) {
         fixture = fixture.item;
+        console.log(fixture.homeTeam.team_name);
 
         return (
-            <View flexDirection={'row'} style={{justifyContent: 'center', alignItems: 'center'}}>
-                <Text>{fixture.id}</Text>
+            // displays dimensions but blank text!ASDNLKASNFLKNADLFKN
+            <View width={itemWidth} flexDirection={'row'} style={{justifyContent: 'space-around', alignItems: 'stretch'}}>
+                <Text style={{borderWidth: 1}}>{fixture.homeTeam.team_name}</Text>
+                <Text style={{alignSelf: 'center', borderWidth: 1}}>{fixture.status}</Text>
+                <Text style={{borderWidth: 1}}>{fixture.awayTeam.team_name}</Text>
             </View>
             );
             // <Image
@@ -31,7 +43,7 @@ export const FixturesScreen = props => {
                     // initialScrollIndex={4}
                     data={league.item}
                     renderItem={renderItem}
-                    keyExtractor={(item,index) => item.id}
+                    keyExtractor={(item,index) => item.id.toString()}
                     />
                 </View>
             }
