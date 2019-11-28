0ee0f67d611c2413583eb1b1c81b0e3fd3844c4f
edoardo sella <eddy.sella@gmail.com>
2019-11-28 17:11:52 +0000

    redux examples

---
+++ redux/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
+import App from './components/App'
+
+const store = createStore(rootReducer)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
+
+// this will be App.js
