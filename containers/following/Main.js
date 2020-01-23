db6bc01a4800c965d3e3947257ea9b3755af872a
edoardo sella <eddy.sella@gmail.com>
2020-01-23 15:26:47 +0000

    following container

---
+++ containers/following/Main.js
@@ -0,0 +1,29 @@
+import React,{ Component }from 'react';
+import { removeLeagueFromFollowing, removeTeamFromFollowing  } from '../../redux/creators/following'
+import { connect } from 'react-redux'
+import { View } from 'react-native'
+import { Main } from '../../screens/following/Following';
+
+const mapStateToProps = state => ({
+    teamIDs: state.followingTeamIDs,
+    leagueIDs: state.followingLeagueIDs,
+    teams: state.teamsByID,
+    leagues: state.leaguesByID,
+)};
+
+const mapDispatchToProps = dispatch => ({
+  removeTeam: input => dispatch(removeTeamFromFollowing(input)),
+  removeLeague: input => dispatch(removeLeagueFromFollowing(input)),
+})
+
+const Screen = ({teamIDs, leagueIDs, teams, leagues, removeTeam,
+  removeLeague, ...props}) => (
+  <View style={{flex:1}}>
+    <Main screenFlex={9} topBarFlex={1} teamIDs={teamIDs} leagueIDs={leagueIDs}
+    teams={teams} leagues={leagues} removeTeam={removeTeam}
+    removeLeague={removeLeague} navigation={props.navigation}/>
+  </View>
+)
+
+
+export default connect(mapStateToProps,mapDispatchToProps)(Screen);
