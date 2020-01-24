e554d6960be8151ecd925f67f746df2a0d6bf20c
edoardo sella <eddy.sella@gmail.com>
2020-01-24 17:32:14 +0000

    sample screen

--- screens/sample.js
+++ screens/sample.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={{flex:1}}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+import React, {Component} from 'react';
+import { ExpoConfigView } from '@expo/samples';
+
+export default function SearchScreen() {
+  /**
+   * Go ahead and delete ExpoConfigView and replace it with your content;
+   * we just wanted to give you a quick view of your config.
+   */
+  return <ExpoConfigView />;
 }
+
+SearchScreen.navigationOptions = {
+  title: 'Search',
+};
