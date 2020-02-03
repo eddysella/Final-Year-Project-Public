187271b18fa65a5bbd601b0a0c5e9251ab3e7be7
edoardo sella <eddy.sella@gmail.com>
2020-02-03 18:14:00 +0000

    new fixtures reducer: future and past fetching var, pagination, and fixturesByID

---
+++ redux/reducers/fixturesV2.js
@@ -0,0 +1,114 @@
+import {
+  STORE_FIXTURES,
+  REQUEST_PAST_FIXTURES,
+  RECEIVE_PAST_FIXTURES,
+  REQUEST_FUTURE_FIXTURES,
+  RECEIVE_FUTURE_FIXTURES,
+  RESET_FIXTURES,
+} from '../types'
+
+function mergeIDs(previous = [], additional){
+  return [...previous, ...additional];
+}
+
+export function fixturesPagination(state={
+  futureNextPage: 1,
+  pastNextPage: 1,
+}, action){
+  switch(action.type){
+    case INCREASE_PAST_PAGE:
+      return Object.assign({}, state, { pastNextPage: (state.pastNextPage+1) });
+    case INCREASE_FUTURE_PAGE:
+      return Object.assign({}, state, { futureNextPage: (state.futureNextPage+1) });
+    case RESET_FIXTURES:
+      return Object.assign({}, state, { futureNextPage: 1, pastNextPage: 1, });
+    default:
+      return state;
+  }
+}
+
+export function fixturesPast(state=false, action){
+  switch(action.type){
+    case REQUEST_PAST_FIXTURES:
+      return true;
+    case RECEIVE_PAST_FIXTURES:
+      return false;
+    default:
+      return state;
+  }
+}
+
+export function fixturesFuture(state=false, action){
+  switch(action.type){
+    case REQUEST_FUTURE_FIXTURES:
+      return true;
+    case RECEIVE_FUTURE_FIXTURES:
+      return false;
+    default:
+      return state;
+  }
+}
+
+export function fixturesByID(state={}, action){
+  switch(action.type){
+    case STORE_FIXTURES:
+      return Object.assign({}, state, action.fixtures);
+    default:
+      return state;
+  }
+}
+
+
+
+// REQUEST_PAST_LEAGUE_FIXTURES,
+// RECEIVE_PAST_LEAGUE_FIXTURES,
+// REQUEST_PAST_TEAM_FIXTURES,
+// RECEIVE_PAST_TEAM_FIXTURES,
+// REQUEST_FUTURE_LEAGUE_FIXTURES,
+// RECEIVE_FUTURE_LEAGUE_FIXTURES,
+// REQUEST_FUTURE_TEAM_FIXTURES,
+// RECEIVE_FUTURE_TEAM_FIXTURES,
+//
+// export function fixturesPastLeague(state={
+//   fetching: false
+//   }, action){
+//   switch(action.type){
+//     case REQUEST_PAST_LEAGUE_FIXTURES:
+//       return Object.assign({}, state, { fetching: true });
+//     case RECEIVE_PAST_LEAGUE_FIXTURES:
+//       return Object.assign({}, state, action.fixtures, { fetching: false });
+//     default:
+//       return state;
+//   }
+// }
+//
+// export function fixturesFuture(state={
+//   fetchingLeagueFixtures: false,
+//   fetchingTeamFixtures: false,
+//   }, action){
+//   switch(action.type){
+//     case REQUEST_FUTURE_FIXTURES:
+//       return Object.assign({}, state,
+//         {
+//           fetchingLeagueFixtures: true,
+//           fetchingTeamFixtures: true,
+//         });
+//     case RECEIVE_FUTURE_LEAGUE_FIXTURES:
+//       return Object.assign({}, state,
+//         {
+//           fetchingLeagueFixtures: false,
+//           [action.timeStamp]: [...state[action.timeStamp], ...action.fixtureIDs]
+//         }
+//       );
+//     case RECEIVE_FUTURE_TEAM_FIXTURES:
+//       return Object.assign({}, state,
+//         {
+//           fetchingTeamFixtures: false,
+//           [action.timeStamp]: [...state[action.timeStamp], ...action.fixtureIDs]
+//         }
+//       );
+//     default:
+//       return state;
+//   }
+// }
+//
