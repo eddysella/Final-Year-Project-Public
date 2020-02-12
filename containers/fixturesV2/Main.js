4499fb2fa29f4c47e18058ec5185ad238af20138
edoardo sella <eddy.sella@gmail.com>
2020-02-12 17:18:17 +0000

    fixtures container

--- containers/fixturesV2/Main.js
+++ containers/fixturesV2/Main.js
@@ -1,9 +1,10 @@
 import React,{ Component }from 'react';
 import { fetchFollowingFutureFixtures, } from '../../redux/creators/futureFixtures'
 import { fetchFollowingPastFixtures , } from '../../redux/creators/pastFixtures'
+import { fetchSpecificFixture , } from '../../redux/creators/fixturesSpecific'
 import { connect } from 'react-redux'
 import { View } from 'react-native'
-import { Main } from '../../screens/fixtures/main/Main';
+import { Main } from '../../screens/fixturesV2/main/Main';
 
 const mapStateToProps = state => ({
     curPastDates: state.fixturesStatus['currentPastDates'],
@@ -11,17 +12,20 @@ const mapStateToProps = state => ({
     futureFetch: state.fixturesStatus['futureFetch'],
     pastFetch: state.fixturesStatus['pastFetch'],
     fixturesByID: state.fixturesByID,
-    fixtureIDs: state.fixturesIDsByDateLeague,
+    fixtureIDs: state.fixtureIDsByDateLeague,
 })
 
 const mapDispatchToProps = dispatch => ({
+  fetchSpecificFixture: (input) => dispatch(fetchSpecificFixture(input)),
   fetchMoreFuture: () => dispatch(fetchFollowingFutureFixtures()),
   fetchMorePast: () => dispatch(fetchFollowingPastFixtures()),
 })
 
+// forgot to add functions ^^ !!!!!!!!
 const Screen = ({curPastDates, curFutureDates, futureFetch, pastFetch,
-  fixturesByID, fixtureIDs, ...props}) => (
+  fixturesByID, fixtureIDs, fetchSpecificFixture, ...props}) => (
   <Main topBarFlex={1} screenFlex={7} curPastDates={curPastDates}
+  curFutureDates={curFutureDates} fetchSpecificFixture={fetchSpecificFixture}
     futureFetch={futureFetch} pastFetch={pastFetch}
     fixturesByID={fixturesByID} fixtureIDs={fixtureIDs}
     navigation={props.navigation}/>
