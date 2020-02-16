a4d29e781d1fcf0bbe25a16a43782ac56c35b7e1
edoardo sella <eddy.sella@gmail.com>
2020-02-16 12:13:21 +0000

    removed code which as making the status bar longer.

--- App.js
+++ App.js
@@ -30,7 +30,6 @@ export default function App(props) {
   } else {
     return (
         <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
           <Provider store={store}>
           <AppNavigator/>
           </Provider>
@@ -67,7 +66,6 @@ function handleFinishLoading(setLoadingComplete) {
 const styles = StyleSheet.create({
   container: {
     flex:1,
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
 
