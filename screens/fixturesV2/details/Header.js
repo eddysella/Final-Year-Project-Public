ea57ddbc67b119414f49e37379b2a6d2bd3cbf71
edoardo sella <eddy.sella@gmail.com>
2020-02-29 11:11:15 +0000

    Spec fixtures screen fix. Forgot to fetch fixture using id.

--- screens/fixturesV2/details/Header.js
+++ screens/fixturesV2/details/Header.js
@@ -4,13 +4,14 @@ import {Image, View, Text,} from 'react-native';
 export const Header = props => {
 
   fixtureID = props.navigation.getParam('id');
+  fixture = props.fixturesByID[fixtureID];
   homeTeam = fixture.homeTeam;
   awayTeam = fixture.awayTeam;
 
   return (
 
-    <View flexDirection={'row'} style={{flex:props.headerFlex, justifyContent: 'space-around', paddingTop:10,}}>
-      <View style={{flex:1, margin: 5}}>
+    <View flexDirection={'row'} style={{flex:props.headerFlex, justifyContent: 'space-around', paddingTop:10, paddingHorizontal:10}}>
+      <View style={{flex:2, margin: 5}}>
         <Image
         style={{flex:2}}
         resizeMode={"contain"}
@@ -23,7 +24,7 @@ export const Header = props => {
         <Text style={{fontSize:20}}>{fixture.status}</Text>
       </View>
 
-      <View style={{flex:1, margin: 5}}>
+      <View style={{flex:2, margin: 5}}>
         <Image
         style={{flex:2}}
         resizeMode={"contain"}
